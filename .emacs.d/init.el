2ae82cc192610f91edea582633c4e28813ad397c
Yu Lu <yulu@utexas.edu>
2019-09-08 19:22:53 +0800

    update new config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,26 +6,64 @@
 
 (add-to-list 'load-path "~/.emacs.d/custom/")
 
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(
+    ;; -------------------------- global configurations -------------------------------------
+    better-defaults      ;; some nice default settings to start with
+    volatile-highlights  ;; highlight yank (auto run) 
+    golden-ratio         ;; used golden ratio when split windows (auto run)
+    highlight-symbol     ;; hight symble and cycling through defs (C-c l activate, M-p/M-n nevigate)
+    auto-complete        ;; auto complete text that has appeared prev, and func auto complete for elisp but not python [disabled]
+    ;; -------------------------- global configurations -------------------------------------
+    elpy                 ;; python environment 
+    ))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+;; --------------------------------------
+
+;; (setq inhibit-startup-message t) ;; hide the startup message
+;; (load-theme 'solarized-light t) ;; load material theme
+;; (global-linum-mode t) ;; enable line numbers globally
+
+
+
+
+
 (require 'yulu-editing)
 (require 'yulu-convenience)
 (require 'yulu-environment)
 (require 'yulu-faces-and-ui)
 (require 'yulu-python3)
 (require 'yulu-latex)
-(require 'yulu-org)
-(require 'yulu-header)
+;; (require 'yulu-org)
+;; (require 'yulu-header)
 
+
+;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (header2 markdown-mode volatile-highlights undo-tree jedi ibuffer-vc highlight-symbol golden-ratio flycheck elpy auctex))))
+ '(company-auto-complete nil)
+ '(company-idle-delay 0.5)
+ '(package-selected-packages (quote (material-theme better-defaults))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(company-scrollbar-bg ((t (:background "color-17"))))
+ '(company-scrollbar-fg ((t (:background "color-243"))))
+ '(company-tooltip ((t (:background "color-110" :foreground "black"))))
+ '(company-tooltip-annotation ((t (:foreground "magenta"))))
+ '(company-tooltip-common ((t (:foreground "brightblue"))))
+ '(company-tooltip-selection ((t (:background "blue")))))
--- README.md
+++ README.md
@@ -1,10 +1,27 @@
-# dotfiles  
-----  
-Dotfiles for my Arch/OpenSUSE Linux, include:
-+ i3 wm
-+ eamcs.d
-+ Xresources
-+ bashrc
-+ Xinit
-+ netctl ut wifi config
+# Julian Lu's Emacs Configuration  
 
+## Folder Structure  
+-emacs.d/
+--init.el 
+--custom/
+---yulu-faces-and-ui.el
+--elpa/
+
+## Configurations  
+### faces-and-ui.el  
++ better-defaults: Some nice defuault settings to start with a emacs configuration
+  - Activate: auto
+  - Effective: gobal 
++ volatile-highlights: Hightlight text when yank, help remind the text just yanked
+  - Activate: auto
+  - Effective: global
++ golden-ratio: split windows with the size of 'golden ratio'
+  - Activate: auto
+  - Effective: global
++ highlight-symbol: hight one/few specific symbol (words, commends,...) and keep the highlight on
+  - Activate: C-c l
+  - Deactive: C-c l
+  - Nav to prev: M-p
+  - Nav to next: M-n 
+  - Effective: global
+  
